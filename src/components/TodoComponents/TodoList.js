b1ce9e1bb0d32b50a7b5b62d94591220c6b291a7
Annique Nsabimana <anniquensabimana@gmail.com>
2019-08-05 16:09:40 -0500

    Created Todo.ccs,Todo.js,TodoForm.Js,TodoLis.js,Updated App.js, Added search.js File for strecth, added styling

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,16 @@
-// your components will all go in this `component` directory.
-// feel free to change this component.js into TodoList.js
+import React from "react";
+import "./Todo.css";
+import Todo from "./Todo";
+
+const TodoList = ({ todoData, handleToggleTask }) => (
+  <React.Fragment>
+    <h2>Tasks</h2>
+    <div className="todo-list">
+      {todoData.map(item => (
+        <Todo key={item.id} todo={item} handleToggleTask={handleToggleTask} />
+      ))}
+    </div>
+  </React.Fragment>
+);
+
+export default TodoList;
